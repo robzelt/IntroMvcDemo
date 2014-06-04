5c53efae79a60a6466ea6f0249c32ab9ff2a584e
robzelt <robzelt@robzelt.com>
2014-06-04 15:42:54 -0400

    Added Lab 1 PDF

--- Lab-1/MvcDemo/Models/MockMeetingRepository.cs
+++ Lab-1/MvcDemo/Models/MockMeetingRepository.cs
@@ -87,26 +87,4 @@ namespace MvcDemo
         }
     }
 
-
-
-    //public static class UpdateExtensions
-    //{
-    //    public delegate void Func<TArg0>(TArg0 element);
-
-    //    public static int Update<TSource>(this IEnumerable<TSource> source, Func<TSource> update)
-    //    {
-    //        if (source == null) throw new ArgumentNullException("source");
-    //        if (update == null) throw new ArgumentNullException("update");
-    //        if (typeof(TSource).IsValueType)
-    //            throw new NotSupportedException("value type elements are not supported by update.");
-
-    //        int count = 0;
-    //        foreach (TSource element in source)
-    //        {
-    //            update(element);
-    //            count++;
-    //        }
-    //        return count;
-    //    }
-    //}
 }
