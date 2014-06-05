85b8f1c30517de2b9e963016395e0d8241c564d0
robzelt <robzelt@robzelt.com>
2014-06-05 10:04:19 -0400

    Added test project

---
+++ Lab-1/MvcDemo.Tests/RepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcDemo.Models;
+
+namespace MvcDemo.Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void RepositoryHasData()
+        {
+            MockMeetingRepository repository = new MockMeetingRepository();
+            Assert.IsTrue(repository.GetAll().Count > 0);
+
+        }
+        [TestMethod]
+        public void RepositoryHasNewData()
+        {
+            MockMeetingRepository repository = new MockMeetingRepository();
+            int initialCount = repository.GetAll().Count;
+
+            Meeting meeting = new Meeting() { Title = "Meeting Title" };
+            repository.Add(meeting);
+            Assert.IsTrue(repository.GetAll().Count == initialCount + 1);
+
+        }
+    }
+}
